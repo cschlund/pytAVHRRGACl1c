6b2348ff6e07f10525d2ae954c4bbdc180077352
Dr. Cornelia Schlundt <cornelia.schlundt@dwd.de>
2015-03-27 08:47:26 +0100

    new regions added

--- regionslist.py
+++ regionslist.py
@@ -12,13 +12,31 @@ REGIONS = dict(
          "par": [-90, 91, 5],
          "mer": [-180, 181, 10],
          "geo": {"lon_0": 20, "lat_0": 0, "height": 9000e3, "width": 8500e3,
-                 "projection": 'stere', "resolution": 'i'}},
+                 "projection": 'stere', "resolution": 'l'}},
+
+    aus={"nam": "Australia",
+         "par": [-90, 91, 5],
+         "mer": [-180, 181, 10],
+         "geo": {"lon_0": 133, "lat_0": -26, "height": 5000e3, "width": 5500e3,
+                 "projection": 'stere', "resolution": 'l'}},
+
+    sam={"nam": "South America",
+         "par": [-90, 91, 5],
+         "mer": [-180, 181, 10],
+         "geo": {"lon_0": -60, "lat_0": -20, "height": 9000e3, "width": 7000e3,
+                 "projection": 'stere', "resolution": 'l'}},
+
+    nam={"nam": "North America",
+         "par": [-90, 91, 5],
+         "mer": [-180, 181, 10],
+         "geo": {"lon_0": -95, "lat_0": 45, "height": 9000e3, "width": 9000e3,
+                 "projection": 'stere', "resolution": 'l'}},
 
     eur={"nam": "Europe",
          "par": [-90, 91, 5],
          "mer": [-180, 181, 10],
          "geo": {"lon_0": 10, "lat_0": 52, "height": 4000e3, "width": 4000e3,
-                 "projection": 'stere', "resolution": 'i'}},
+                 "projection": 'stere', "resolution": 'l'}},
 
     ger={"nam": "Germany",
          "par": [-90, 91, 2],
@@ -32,9 +50,27 @@ REGIONS = dict(
          "geo": {"llcrnrlat": -90, "urcrnrlat": 90, "llcrnrlon": -180, "urcrnrlon": 180,
                  "projection": 'cyl', "resolution": 'c'}},
 
+    ame={"nam": "America",
+         "par": [-90, 91, 30],
+         "mer": [-180, 181, 60],
+         "geo": {"llcrnrlat": -60, "urcrnrlat": 70, "llcrnrlon": -170, "urcrnrlon": -15,
+                 "projection": 'cyl', "resolution": 'c'}},
+
     rus={"nam": "Russia",
          "par": [-90, 91, 5],
          "mer": [-180, 181, 60],
          "geo": {"lon_0": 100, "lat_0": 70, "height": 5000e3, "width": 9000e3,
-                 "projection": 'stere', "resolution": 'i'}}
+                 "projection": 'stere', "resolution": 'i'}},
+
+    npol={"nam": "North Pole",
+          "par": [-80., 81., 20.],
+          "mer": [-180, 181, 20.],
+          "geo": {"boundinglat": 60, "lon_0": 270,
+                  "projection": 'npstere', "resolution": 'l'}},
+
+    spol={"nam": "South Pole",
+          "par": [-80., 81., 20.],
+          "mer": [-180, 181, 20.],
+          "geo": {"boundinglat": -60, "lon_0": 90,
+                  "projection": 'spstere', "resolution": 'l'}}
 )
